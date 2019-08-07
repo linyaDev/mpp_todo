3473703065f829db6b79063ec190e0e2445c3997
Ilya <nil.koder@gmail.com>
2019-08-07 08:33:38 +0300

    android classes refactoring

--- share/src/commonMain/kotlin/mvi/Storage.kt
+++ share/src/commonMain/kotlin/mvi/Storage.kt
@@ -2,16 +2,10 @@ package com.linya.utils.mvi
 
 import com.linya.utils.dispatcher
 import com.linya.utils.interfaces.RenderView
-import com.linya.utils.interfaces.RouterDependencies
-import com.linya.utils.interfaces.ViewType
-import com.linya.utils.router.Router
-import com.linya.utils.ui.todo.TodoMainStorage
-import com.linya.utils.ui.todo.TodoRouter
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.channels.consumeEach
-import kotlin.reflect.KClass
 
 
 typealias Store<Wish,State> = Storage<Wish,State,*,*>
@@ -23,14 +17,14 @@ abstract class Storage<Wish : Any ,State : Any,Effect : Any,News: Any>(
         private val reducer: Reducer<State,Effect>,
         private val newsPublisher: NewsPublisher<Wish,State,Effect,News>? = null
 ){
-    private val subject = ConflatedBroadcastChannel<State>()
-    private val channel = Channel<Pair<Wish,Effect>>()
+    private val stateChannel = ConflatedBroadcastChannel<State>()
+    private val wishChannel = Channel<Pair<Wish,Effect>>()
 
     init {
 
         GlobalScope.launch(dispatcher) {
-            subject.send(initState)
-            subject.openSubscription().consumeEach {
+            stateChannel.send(initState)
+            stateChannel.openSubscription().consumeEach {
                 this@Storage.renderView.render(it)
             }
         }
@@ -57,9 +51,9 @@ abstract class Storage<Wish : Any ,State : Any,Effect : Any,News: Any>(
 
         GlobalScope.launch(dispatcher) {
 
-            for(pair in channel){
-                val newState = reducer.reduce(subject.value,pair.second)
-                subject.send(newState)
+            for(pair in wishChannel){
+                val newState = reducer.reduce(stateChannel.value,pair.second)
+                stateChannel.offer(newState)
                 val news = newsPublisher?.invokeNewsPublisher(pair.first,pair.second,newState)
                 if(news!=null){
                     renderView.processNews(news)
@@ -72,12 +66,16 @@ abstract class Storage<Wish : Any ,State : Any,Effect : Any,News: Any>(
         val emitter = object : Emitter<Effect>{
             override fun send(effect: Effect) {
                 GlobalScope.launch(dispatcher) {
-                    channel.send(Pair(wish,effect))
+                    wishChannel.send(Pair(wish,effect))
                 }
             }
         }
 
-        actor.invoke(subject.value,wish,emitter)
+        actor.invoke(stateChannel.value,wish,emitter)
     }
 
+    fun dismiss(){
+        wishChannel.close()
+        stateChannel.close()
+    }
 }
