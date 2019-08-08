65f70b66ce7a25cc89282b4ada721f0d0a02aa28
Ilya Nikolaev <ilya.nikolaev@ntprog.com>
2019-08-08 13:25:18 +0300

    add missing files

---
+++ iosApp/iosApp/ui/todo/cells/TodoCell.swift
@@ -0,0 +1,25 @@
+//
+//  TodoCell.swift
+//  iosApp
+//
+//  Created by Ilya Nikolaev on 01/08/2019.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    @IBOutlet var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        //super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    
+    
+}
